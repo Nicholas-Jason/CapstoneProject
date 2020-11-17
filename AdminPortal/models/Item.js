63ef8ea2211ae4c4daf2659ef8ee01b5ef1070f4
Nicholas-Jason Roache <njrr325@gmail.com>
2020-11-17 17:18:25 -0500

    Add files via upload

---
+++ AdminPortal/models/Item.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.pluralize(null);
+const ItemSchemaRef = mongoose.Schema({
+    _id:Number,
+    iname:String,
+    price:Number,
+    image:String,
+    company:String
+});
+
+module.exports = mongoose.model("Item",ItemSchemaRef);
